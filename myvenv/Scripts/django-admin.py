17cbd37950b097eb79b3bf1ece6cc640e2dde716
Choi-JungWon <0401ah@gmail.com>
2019-07-30 08:22:59 +0900

    first commit of blog project

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\choi jungwon\desktop\second\firstproject\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
